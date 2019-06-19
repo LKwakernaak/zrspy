6682ff27ed08579614bdaba95b31f8e92cf6c71e
Kwakernaak <lennardkwakernaak@gmail.com>
2019-06-19 22:24:31 +0200

    Initial commit
    Everything that worked in the past.
    
    Not included is the google api token file. This will be deprecated when moving to .ics files.

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+      name='zrspy',
+      version='0.1',
+      py_modules=['zrs'],
+      install_requires=[
+              'click',
+              'bs4',
+              'pandas',
+              'httplib2',
+              'datetime',
+              'requests',
+              'sqlalchemy',
+              'oauth2client',
+              'googleapiclient',
+              ],
+      entry_points='''
+      [console_scripts]
+      zrspy=zrs:main
+      ''',
+      )
+
