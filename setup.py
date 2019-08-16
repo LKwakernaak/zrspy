a5ee1312a3db1eba15d6b49ffc98d11abaeee6e6
Lennard Kwakernaak <lennardkwakernaak@gmail.com>
2019-08-16 15:16:59 +0200

    Autodetect required packages

--- setup.py
+++ setup.py
@@ -1,24 +1,27 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
-      name='zrspy',
-      version='0.1',
-      py_modules=['zrs'],
-      install_requires=[
-              'click',
-              'bs4',
-              'pandas',
-              'httplib2',
-              'datetime',
-              'requests',
-              'sqlalchemy',
-              'oauth2client',
-              'google-api-python-client',
-			  'lxml',
-              ],
-      entry_points='''
-      [console_scripts]
-      zrspy=zrs:main
-      ''',
-      )
+    name='zrspy',
+    version='0.1',
+    py_modules=['zrs'],
+    packages=find_packages(),
+    # install_requires=[
+    #         'click',
+    #         'bs4',
+    #         'pandas',
+    #         'httplib2',
+    #         'datetime',
+    #         'requests',
+    #         'sqlalchemy',
+    #         'oauth2client',
+    #         'google-api-python-client',
+    # 	  'lxml',
+    #         ],
+    entry_points='''
+    [console_scripts]
+    zrspy=zrs:main
+    ''',
+    author='l.kwakernaak',
+    author_email='kwakernaak@physics.leidenuniv.nl'
+)
 
